c20b49674eea25915d82293d5b35fd8b4e127746
Lamees AlFallaj <lafallaj2@gmail.com>
2019-04-28 18:27:11 +0300

    easier than the assignment

--- weather/script.js
+++ weather/script.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-}); 
+    $('#submit').on('click', function(event) {
+
+        var zip = $('#zipcode').val();
+        console.log(zip)
+        console.log('hi')
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&units=metric&appid=28ab32de8c4f582c7b7c6b204686db1e',
+            success: function(result) {
+                console.log(result)
+                $('#result').append($('<p/>').text('City: ' + result.name))
+                $('#result').append($('<p/>').text('temperature: ' + result.main.temp + 'C'))
+                $('#result').append($('<p/>').text('description: ' + result.weather[0].main))
+                $('#result').append($('<p/>').text('max-temp: ' + result.main.temp_max))
+                $('#result').append($('<p/>').text('min-tamp: ' + result.main.temp_min))
+            },
+            error: function(result) {}
+        })
+        event.preventDefault();
+    })
+
+
+})
