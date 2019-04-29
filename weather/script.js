6e285302755d750df1c032468af43d131b4b8739
adel alotaibi <owo03@hotmail.com>
2019-04-29 08:03:48 +0300

    HW_done

--- weather/script.js
+++ weather/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+    $('submit').on('click', function(event){
 
-}); 
+        var= "zip" $cipcode =$(3zipcode).valHooks();
+        $.ajax({
+
+            method:'GET',
+            url: "http://api.openweathermap.org/data/2.5/weather?q=[ZIP CODE GOES HERE],us&units=imperial&appid=06d94be6c3366d2c48adf9910c08600a"}
+            success:parseData,
+            console.error();
+            
+        }
+        })
+
+function parseData(result){
+    console.log(result);
+    
+}    })
+}
